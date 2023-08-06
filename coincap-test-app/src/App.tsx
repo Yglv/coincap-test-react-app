a1b06ab85cec2950d45c7100d59993ac9a4898a8
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-08-06 23:42:56 +0300

    feat: add MainMenu

--- coincap-test-app/src/App.tsx
+++ coincap-test-app/src/App.tsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import MainMenu from '@/pages/MainMenu/MainMenuPage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     
+     <MainMenu/>
     </>
   )
 }
