ed0b98fadbe3057d11c0b3db9ac14f8c80c3aac0
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-08-07 23:41:02 +0300

    feat: add Modal and Context

--- coincap-test-app/src/pages/MainMenu/MainMenuPage.tsx
+++ coincap-test-app/src/pages/MainMenu/MainMenuPage.tsx
@@ -1,13 +1,22 @@
 import CoinTable from "@/components/tables/CoinTable/CoinTable"
 import Header from "@/components/layout/Header/Header";
-import { ReactElement } from "react";
+import { MouseEvent, ReactElement, useState } from "react";
+import Modal from "@/components/layout/Modal/Modal";
+import { Context } from '@/context/context'
 
 function MainMenu(): ReactElement{
+  const [isActive, setIsActive] = useState<boolean>(false)
   return (
-  <>
+  <Context.Provider value={{setIsActive}}>
     <Header/>
+    <Modal title='Добавить в портфель' isActive={isActive} onClose={() => setIsActive(false)}>
+        <form className="modal_form">
+          <input type="number" placeholder="Введите сумму..." name="sum" className="modal_input" />
+          <button type="submit" className="modal_button">Добавить</button>
+        </form>
+    </Modal>
     <CoinTable/>
-  </>
+  </Context.Provider>
   )
 }
 
