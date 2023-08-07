ec5645ec5163a3e44ecb7cc059cd6249bb0961cc
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-08-07 23:45:18 +0300

    feat: add CoinInfo page

---
+++ coincap-test-app/src/pages/CoinInfo/CoinInfoPage.tsx
@@ -0,0 +1,11 @@
+import CoinTable from "@/components/tables/CoinTable/CoinTable"
+import Header from "@/components/layout/Header/Header";
+import { MouseEvent, ReactElement, useState } from "react";
+import Modal from "@/components/layout/Modal/Modal";
+import { Context } from '@/context/context'
+
+function CoinInfo(){
+  return (<div></div>)
+}
+
+export default CoinInfo;
