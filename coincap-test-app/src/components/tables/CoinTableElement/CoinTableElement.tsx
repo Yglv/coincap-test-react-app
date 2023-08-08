96323b1cb01356cc6bcf733acb4d9694dab29846
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-08-08 21:47:02 +0300

    feat: add link to coin info page

--- coincap-test-app/src/components/tables/CoinTableElement/CoinTableElement.tsx
+++ coincap-test-app/src/components/tables/CoinTableElement/CoinTableElement.tsx
@@ -9,12 +9,16 @@ function CoinTableElement(props: ICoinTableElement): ReactElement{
   return (
     <>
       <p className="cointable_element_item">{props.num}</p>
-      <p className="cointable_element_item">{props.name}</p>
-      <p className="cointable_element_item">{props.price} $</p>
-      <p className="cointable_element_item">{props.hour}%</p>
-      <p className="cointable_element_item">{props.day}%</p>
-      <p className="cointable_element_item">{props.volume} $</p>
-      <p className="cointable_element_item">{props.capitalization} $</p>
+      <p className="cointable_element_item">
+        <a href={`http://127.0.0.1:5173/info/${props.num}`} className="cointable_element_item_link">
+          {props.name}  
+        </a>
+      </p>
+      <p className="cointable_element_item">${props.price}</p>
+      {(props.day[0] === '-' || props.day === '0') ? <p className="cointable_element_item cointable_element_item-red">{props.day}%</p>
+                                    : <p className="cointable_element_item cointable_element_item-green">{props.day}%</p>}
+      <p className="cointable_element_item">${props.volume}</p>
+      <p className="cointable_element_item">${props.capitalization}</p>
       <button onClick={() => setIsActive(true)} className="cointable_element_button"><i className="fa-solid fa-plus"></i></button>
     </>
   )
