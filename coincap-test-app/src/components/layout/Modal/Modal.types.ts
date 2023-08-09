8e709cb73f35b33d7adacfec5517a945aea82f72
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-08-09 14:25:55 +0300

    fix: replace Modal component to forms dir

--- coincap-test-app/src/components/layout/Modal/Modal.types.ts
+++
@@ -1,8 +0,0 @@
-import { MouseEventHandler, ReactElement } from "react";
-
-export interface IModal{
-  title: string,
-  children?: ReactElement,
-  isActive?: boolean,
-  onClose?: MouseEventHandler<HTMLSpanElement> | undefined
-}
