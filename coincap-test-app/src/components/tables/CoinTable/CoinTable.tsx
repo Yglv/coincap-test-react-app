610e8a24e990aa4f7ae45f894f2300969b2c6ce5
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-08-09 13:24:07 +0300

    fix: delete 1 hour field

--- coincap-test-app/src/components/tables/CoinTable/CoinTable.tsx
+++ coincap-test-app/src/components/tables/CoinTable/CoinTable.tsx
@@ -8,7 +8,7 @@ import Pagination from "@/components/layout/Pagination/Pagination"
 function CoinTable(): ReactElement{
   const APIURL = 'https://api.coincap.io/v2/assets'
   const [coinData, setCoinData] = useState<ICoinData[]>([])
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1)
   const [coinsPerPage] = useState(10)
   const lastCoinIndex = currentPage * coinsPerPage
   const firstCoinIndex = lastCoinIndex - coinsPerPage
@@ -33,18 +33,17 @@ function CoinTable(): ReactElement{
           <p className="cointable_header_item">#</p>
           <p className="cointable_header_item">Наименование</p>
           <p className="cointable_header_item">Цена</p>
-          <p className="cointable_header_item">1 ч</p>
           <p className="cointable_header_item">24 ч</p>
           <p className="cointable_header_item">Объём торгов за 24 часа</p>
           <p className="cointable_header_item">Рыночная капитализация</p>
         </div>
         {currentCoins.map(coin => {
           return (<div className="cointable_element">
-            <CoinTableElement num={coin.rank} name={coin.name} price={Number(coin.priceUsd).toFixed(2)} hour={Number(coin.vwap24Hr).toFixed(2)} day={Number(coin.volumeUsd24Hr).toFixed(2)} volume={Number(coin.volumeUsd24Hr).toFixed(2)} capitalization={Number(coin.marketCapUsd).toFixed(2)}/>
+            <CoinTableElement id={coin.id} num={coin.rank} name={coin.name} symbol={coin.symbol} price={Intl.NumberFormat("ru-Ru").format(+Number(coin.priceUsd).toFixed(2))} day={Intl.NumberFormat("ru-Ru").format(+Number(coin.changePercent24Hr).toFixed(2))} volume={Intl.NumberFormat("ru-Ru").format(+Number(coin.volumeUsd24Hr).toFixed(0))} capitalization={Intl.NumberFormat("ru-Ru").format(+Number(coin.marketCapUsd).toFixed(0))}/>
           </div>)
         })}
         <Pagination coinsPerPage={coinsPerPage} totalCoins={coinData.length} paginate={paginate}/>
-    </div>
+      </div>
     </>
   )
 }
