ad14b97bfc86cabbec85f17c5cf4727034d9682f
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-08-10 11:01:21 +0300

    fix: replace handleClick function to Portfolio service

--- coincap-test-app/src/pages/MainMenu/MainMenuPage.tsx
+++ coincap-test-app/src/pages/MainMenu/MainMenuPage.tsx
@@ -1,40 +1,38 @@
 import CoinTable from "@/components/tables/CoinTable/CoinTable"
 import Header from "@/components/layout/Header/Header";
-import { ReactElement, useContext, useRef, useState } from "react";
+import { ReactElement, useEffect, useRef, useState } from "react";
 import Modal from "@/components/forms/Modal";
-import { CoinContext, Context } from '@/context/context'
-import { ICoinData } from "@/components/tables/CoinTable/CoinTable.types";
+import { Context } from '@/context/context'
+import axios from "axios";
+import PortfolioService from "@/services/PortfolioService";
 
 function MainMenu(): ReactElement{
   const [isActive, setIsActive] = useState<boolean>(false)
   const [name, setName] = useState<string>('')
-  const [price, setPrice] = useState<string>(0)
+  const [id, setId] = useState<string>('')
+  const [price, setPrice] = useState<number>(0)
   const numInputRef = useRef(null)
-
-  const handleClick = () => {
-    const title = name
-    console.log(+numInputRef.current.value)
-    console.log(parseFloat(price.replace(/\s/g,'')))
-    let sum = +numInputRef.current.value * parseFloat(price.replace(/\s/g,''))
-    console.log(sum)
-    if (localStorage.getItem(title)){
-      sum += +localStorage.getItem(title)
-    }
-    localStorage.setItem(title, String(sum))
-  }
+  useEffect(() => {
+    axios
+    .get(`https://api.coincap.io/v2/assets/${id}`)
+    .then(res => {
+      console.log(res.data.data.priceUsd)
+      setPrice(res.data.data.priceUsd)
+    })
+  })
 
   return (
-  <Context.Provider value={{setIsActive, setName, setPrice}}>
-    <Header/>
-    <Modal title='Добавить в портфель' isActive={isActive} onClose={() => setIsActive(false)}>
-        <form className="modal_form">
-          <input type="text" disabled value={name} name="title" className="modal_input" />
-          <input type="number" ref={numInputRef} placeholder="Введите сумму..." name="sum" className="modal_input" />
-          <button type="submit" onClick={() => handleClick()} className="modal_button">Добавить</button>
-        </form>
-    </Modal>
-    <CoinTable/>
-  </Context.Provider>
+    <Context.Provider value={{setIsActive, setName, setId}}>
+      <Header/>
+      <Modal title='Добавить в портфель' isActive={isActive} onClose={() => setIsActive(false)}>
+          <form className="modal_form">
+            <input type="text" disabled value={name} name="title" className="modal_input" />
+            <input type="number" ref={numInputRef} placeholder="Введите сумму..."  name="sum" className="modal_input" />
+            <button  onClick={() => PortfolioService.addToPortfolio(name, +numInputRef.current.value, price)} className="modal_button">Добавить</button>
+          </form>
+      </Modal>
+      <CoinTable/>
+    </Context.Provider>
   )
 }
 
