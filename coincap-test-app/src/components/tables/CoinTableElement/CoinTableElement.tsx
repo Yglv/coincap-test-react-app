c5ed47cfa09dd4e30712ef433f39cf089dc27601
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-08-10 11:03:43 +0300

    fix: fix context functions

--- coincap-test-app/src/components/tables/CoinTableElement/CoinTableElement.tsx
+++ coincap-test-app/src/components/tables/CoinTableElement/CoinTableElement.tsx
@@ -5,7 +5,7 @@ import { Context } from '@/context/context'
 import { useContext } from "react"
 
 function CoinTableElement(props: ICoinTableElement): ReactElement{
-  const { setIsActive, setName, setPrice } = useContext(Context)
+  const { setIsActive, setName, setId } = useContext(Context)
   return (
     <>
       <p className="cointable_element_item">{props.num}</p>
@@ -19,7 +19,7 @@ function CoinTableElement(props: ICoinTableElement): ReactElement{
                                     : <p className="cointable_element_item cointable_element_item-green">{props.day}%</p>}
       <p className="cointable_element_item">${props.volume}</p>
       <p className="cointable_element_item">${props.capitalization}</p>
-      <button onClick={() => { setIsActive(true); setName(props.name); console.log(props.price) ;setPrice(props.price)}} className="cointable_element_button"><i className="fa-solid fa-plus"></i></button>
+      <button onClick={() => { setIsActive(true); setName(props.name); setId(props.id)}} className="cointable_element_button"><i className="fa-solid fa-plus"></i></button>
     </>
   )
 }
