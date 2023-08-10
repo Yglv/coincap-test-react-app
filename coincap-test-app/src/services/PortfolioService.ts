3329f41a2ec7c02f5663aa216735269f920088cc
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-08-10 11:36:32 +0300

    feat: add setting last adding sum to localstorage

--- coincap-test-app/src/services/PortfolioService.ts
+++ coincap-test-app/src/services/PortfolioService.ts
@@ -1,6 +1,7 @@
 export default class  PortfolioService{
   static addToPortfolio(title: string, count: number, price: number ):void{
     let sum = count * price
+    localStorage.setItem('lastSum', String(sum))
     if (localStorage.getItem(title)){
       sum += +localStorage.getItem(title)
     }
