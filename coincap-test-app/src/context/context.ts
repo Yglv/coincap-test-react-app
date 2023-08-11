8dbfc60bc5a55fa23848e7c8c3ecc3d9c119d50c
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-08-11 17:46:04 +0300

    feat: add StoreContext

--- coincap-test-app/src/context/context.ts
+++ coincap-test-app/src/context/context.ts
@@ -1,3 +1,10 @@
+import Store from "@/store/Store";
 import React from "react";
 
 export const Context = React.createContext({});
+
+interface State{
+  store: Store
+}
+export const store = new Store()
+export const StoreContext = React.createContext<State>({ store })
