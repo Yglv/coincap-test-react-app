c8e7c590bf86c6cca23e83fe1af1665674951374
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2023-08-11 17:36:25 +0300

    feat: add mobx and context

--- coincap-test-app/src/main.tsx
+++ coincap-test-app/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
+import { StoreContext, store } from './context/context'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App/>
+    <StoreContext.Provider value={{store}}>
+      <App/>
+    </StoreContext.Provider>
   </React.StrictMode>,
 )
